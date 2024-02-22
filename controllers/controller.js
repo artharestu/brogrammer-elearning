89f13d035c2cdf07f1ddf72cef1d4e0f94758b51
Robherto <robhertomatt@gmail.com>
2024-02-22 21:50:51 +0700

    showProfile udah jadi

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 
 class Controller {
   static async home(req, res) {
+    /**menampilkan username di navbar */
     const { username } = req.session;
     try {
       let dataCourse = await Course.findAll({ include: Category });
@@ -11,12 +12,11 @@ class Controller {
       console.log(error);
       res.send(error);
     }
-
   }
 
   static loginPage(req, res) {
     const { username } = req.session;
-    res.render('login', { username })
+    res.render("login", { username });
   }
 
   static async login(req, res) {
@@ -57,6 +57,11 @@ class Controller {
   static registerPage(req, res) {
     const { username } = req.session;
     res.render("register", { username });
+    /** register username harus ada username nya
+     * kerena di nav.ejs nya ada pengandaian yang menggunakan
+     * variabel username. jika tidak ada yang oper dari render
+     * register page maka akan terjadi error
+     */
   }
 
   static async register(req, res) {
@@ -69,6 +74,44 @@ class Controller {
       res.send(error);
     }
   }
+
+  static inputUserProf(req, res) {
+    const { username } = req.session;
+    /** SESSION adalah sebuah built in function nya express
+     * jadi bisa membuat data yang di input dari user bisa tersimpan\ */
+    res.render("inputUserProfile", { username });
+  }
+
+  static async userProf(req, res) {
+    const { username } = req.session;
+    const { fullName, profilePicture, dateOfBirth } = req.body;
+    try {
+      console.log(username);
+      let user = await User.findOne({
+        where: { username },
+      });
+
+      let UserId = user.id;
+      let dataProfile = await UserProfile.create({ fullName, profilePicture, dateOfBirth, UserId });
+      console.log(dataProfile);
+      /** render gapake (/) untuk sebelah kiri <nama file ejs> */
+      res.render("showProfile", { dataProfile, username });
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  }
+
+  // static async showProfile(req, res) {
+  //   // const { username } = req.session;
+  //   try {
+  //     // let dataProfile = await UserProfile.findAll({});
+  //     res.render("showProfile");
+  //   } catch (error) {
+  //     console.log(error.message);
+  //     res.send(error);
+  //   }
+  // }
 }
 
 module.exports = Controller;
--- routers/index.js
+++ routers/index.js
@@ -5,9 +5,21 @@ const router = express.Router();
 
 router.get("/", Middleware.auth, Controller.home);
 router.get("/login", Middleware.login, Controller.loginPage);
+
+/** ini untuk user profile */
+router.get("/userProfile", Middleware.login, Controller.inputUserProf);
+router.post("/userProfile", Middleware.login, Controller.userProf);
+// router.get("/showProfile", Middleware.login, Controller.showProfile);
+
+/** ini untuk user profile */
+
 router.post("/login", Controller.login);
 router.get("/logout", Controller.logout);
 router.get("/register", Middleware.login, Controller.registerPage);
+/** yang controller ga berhubungan sama menampilkan sebuah tampilan
+ * di user maka gaperlu middleware
+ */
+
 router.post("/register", Controller.register);
 
 module.exports = router;
