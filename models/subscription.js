bd0b21aff7d97fdc2e1d531160664f83b0f4b53f
artharestu <artharestuabadi@gmail.com>
2024-02-23 05:12:32 +0700

    Associate

--- models/subscription.js
+++ models/subscription.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Subscription extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Subscription.belongsTo(models.Course)
       Subscription.belongsTo(models.User)
